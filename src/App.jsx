f5e93ba9025e220b453a710aeb0df78437b167cf
[Ahmed <ahmed.king332333@gmail.com>
2026-02-19 02:43:34 +0200

    changes 1.0

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route index element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="app" element={<AppLayout />} />
